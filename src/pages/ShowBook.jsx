7191589f90fca55d90cac3b023c8cb4b51a6f51c
Vinit <singhvinit435@gmail.com>
2024-03-01 23:02:13 +0530

    add details page

--- src/pages/ShowBook.jsx
+++ src/pages/ShowBook.jsx
@@ -8,12 +8,48 @@ const ShowBook = () => {
   const { id } = useParams();
   useEffect(() => {
     axios
-      .get(`http://localhost:4000/api/books/${id}`)
+      .get(`http://localhost:4000/api/v1/books/${id}`)
       .then((response) => setBook(response.data.data))
       .catch((err) => console.log(err));
   }, []);
-  return <>
-  </>;
+  return (
+    <>
+      <div className="border mx-auto my-16 rounded-md pl-4 py-4 w-1/2 flex flex-col gap-y-4 shadow-[5px_5px_0px_0px_rgba(109,40,217)]">
+        <div className="flex">
+          <h1 className="text-xs font-semibold uppercase basis-1/3 text-center">
+            id :
+          </h1>
+          <h1 className="text-xs capitalize">{book._id}</h1>
+        </div>
+        <div className="flex">
+          <h1 className="text-xs font-semibold uppercase basis-1/3 text-center">
+            title :
+          </h1>
+          <h1 className="text-xs capitalize">{book.title}</h1>
+        </div>
+        <div className="flex">
+          <h1 className="text-xs font-semibold uppercase basis-1/3 text-center">
+            author :
+          </h1>
+          <h1 className="text-xs capitalize">{book.author}</h1>
+        </div>
+        <div className="flex">
+          <h1 className="text-xs font-semibold uppercase basis-1/3 text-center">
+            publishYear :
+          </h1>
+          <h1 className="text-xs capitalize">{book.publishYear}</h1>
+        </div>
+        <div className="flex">
+          <h1 className="text-xs font-semibold uppercase basis-1/3 text-center">
+            description :
+          </h1>
+          <p className="text-xs capitalize basis-1/2 text-justify">
+            {book.description}
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ShowBook;
